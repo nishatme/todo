c235c0607734e24e865775431fbb15c8195de7d2
Jaynul A Nishat <omicronicnishat@gmail.com>
2022-12-26 00:07:28 +0600

    first

---
+++ README.md
@@ -0,0 +1,20 @@
+# Todos WebApp
+This is the code for Todos WebApp built in Flask, with a user system built through json files.
+The site is live on http://sggts04.pythonanywhere.com however it is expected to be on a more updated state than this repo.
+
+## Installation
+For testing, editing or playing around with the code, you can download the app to your local machine:
+```
+git clone https://github.com/sggts04/flask-todos
+cd flask-todos
+pip install -r requirements.txt
+```
+Now, edit app.py and set ```app.config['SECRET_KEY'] = "secret_key_here"``` to your own secret key instead of ```secret_key_here```.    
+Then run
+```
+python app.py
+```
+or ```python3 app.py``` or ```python3.7 app.py``` depending on how python is set up on your machine. Please note that this webapp built in Python 3.7    
+Then the app will be available on http://127.0.0.1:5000/ then. However, the live site is [here](http://sggts04.pythonanywhere.com/) which you can't edit :)    
+## Video
+A video demonstrating(an older version) this app: https://www.youtube.com/watch?v=Ecf2_Wc0SxI    
---
+++ app.py
@@ -0,0 +1,183 @@
+from flask import Flask, render_template, request, redirect, session, flash, jsonify
+import json
+import datetime
+from passlib.hash import pbkdf2_sha256
+
+now = datetime.datetime.now()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret_key_here"
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+file = open('todos.json')
+todos = json.load(file)
+file.close()
+file2 = open('users.json')
+users = json.load(file2)
+file2.close()
+
+@app.route('/')
+@app.route('/index')
+def index():
+	global todos
+	if not session.get('logged_in'):
+		return redirect('/login')
+	return render_template('index.html', todo = todos[session['username']])
+
+@app.route('/update', methods=['POST'])
+def update():
+	global todos
+	if not session.get('logged_in'):
+		return redirect('/login')
+	id = request.form['id']
+	todos[session['username']][id][0] = "Done"
+	todos[session['username']][id][4] = datetime.datetime.today().strftime('%d-%m-%Y')
+	file = open('todos.json', 'w')
+	json.dump(todos, file, indent=4)
+	file.close()
+	return redirect('/')
+
+@app.route('/add')
+def add():
+	if not session.get('logged_in'):
+		return redirect('/login')
+	return render_template('add.html')
+
+@app.route('/addtodo', methods=['POST'])
+def addtodo():
+	global todos
+	if not session.get('logged_in'):
+		return redirect('/login')
+	title = request.form['title']
+	if title in todos[session['username']]:
+		flash('A Todo with an Identical Title already exists. Try the title "'+title+' 2'+'"')
+		return redirect('/add')
+	desc = request.form['desc']
+	comdate = request.form['comdate']
+	date = datetime.datetime.today().strftime('%d-%m-%Y')
+	if title=='' or desc=='' or comdate=='':
+		flash("You can't leave any of the fields blank")
+		return redirect('/add')
+	todos[session['username']].update({title: ["Not Done", desc, date, comdate, ""]})
+	file = open('todos.json', 'w')
+	json.dump(todos, file, indent=4)
+	file.close()	
+	return redirect('/')
+
+@app.route('/login')
+def login():
+	if session.get('logged_in'):
+		return redirect('/')
+	return render_template('login.html')
+
+@app.route('/loginattempt', methods=['POST'])
+def loginattempt():
+	attempted_id = request.form['loginid']
+	attempted_pass = request.form['loginpass']
+	if attempted_id=='' or attempted_pass=='':
+		flash("You can't leave any of the fields blank")
+		return redirect('/login')
+	if attempted_id in users:
+		if pbkdf2_sha256.verify(attempted_pass, users[attempted_id]):
+			session['logged_in'] = True
+			session['username'] = attempted_id
+			return redirect('/')
+		else:
+			flash('Wrong Password')
+			return redirect('/login')
+	else:
+		flash('No such username exists')
+		return redirect('/login')
+
+@app.route('/logout')
+def logout():
+	if not session.get('logged_in'):
+		return redirect('/login')
+	session.pop('logged_in', None)
+	session.pop('username', None)
+	return redirect('/login')
+
+@app.route('/register')
+def register():
+	if session.get('logged_in'):
+		return redirect('/')
+	return render_template('register.html')
+
+@app.route('/registerattempt', methods=['POST'])
+def registerattempt():
+	register_id = request.form['registerid']
+	register_pass = request.form['registerpass']
+	if register_id=='' or register_pass=='':
+		flash("You can't leave any of the fields blank")
+		return redirect('/register')
+	if ' ' in register_id:
+		flash("You can't have a space in your username")
+		return redirect('/register')
+	register_pass_hashed = pbkdf2_sha256.hash(register_pass)
+	if register_id in users:
+		flash('Username already taken')
+		return redirect('/register')
+	else:
+		users.update({register_id: register_pass_hashed})
+		todos.update({register_id: {}})
+		session['logged_in'] = True
+		session['username'] = register_id
+		file2 = open('users.json', 'w')
+		json.dump(users, file2, indent=4)
+		file2.close()
+		file = open('todos.json', 'w')
+		json.dump(todos, file, indent=4)
+		file.close()
+		return redirect('/')
+
+@app.route('/api', methods=['GET'])
+def api():
+	try:
+		api_user = request.args.get('user')
+		api_pass = request.args.get('pass')
+		if api_user in users:
+			if pbkdf2_sha256.verify(api_pass, users[api_user]):
+				return jsonify(todos[api_user])
+			else:
+				return "Wrong Password"
+		else:
+			return "User Not Found"
+	except:
+		return "Wrong Syntax"
+
+@app.route('/apidoc')
+def apidoc():
+	return render_template('apidoc.html')
+
+@app.route('/profile')
+def profileredirect():
+	if not session.get('logged_in'):
+		return redirect('/')
+	return redirect('/profile/'+session['username'])
+
+@app.route('/profile/<user>')
+def profile(user):
+	if not user in users:
+		return render_template('nouser.html')
+	todolen = len(todos[user])
+	len1=0
+	for todoname in todos[user]:
+		if todos[user][todoname][0]=="Not Done":
+			len1+=1
+	return render_template('profile.html', user = user, todolen = todolen, todolennd = len1, todolend = todolen-len1)
+
+#Errors
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    app.run()
